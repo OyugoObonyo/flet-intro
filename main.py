4a220820389c9886c1531a273d508d4278b88f4e
OyugoObonyo <nyikal1999@gmail.com>
2022-09-03 12:04:18 +0300

    Modify row control properties

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from time import sleep
 import flet
 from flet import Page, Text, Column, Row
 
 def main(page: Page):
-    t = Text(value="Hello World!", color="red")
     # page.add(x) can be used in place of page.controls.append(x)
-    page.add(t)
-    for i in range(1, 11):
-        t.value = f"Hello world {i}"
-        page.update()
-        sleep(1)
+    page.add(
+        Row(controls = [
+            Text(value="Hello World red!", color="red"),
+            Text(value="Hello World green!", color="green"),
+            Text(value="Hello World blue!", color="blue")
+        ])
+    )
+    page.update()
+
 
 flet.app(target=main, view=flet.WEB_BROWSER)
