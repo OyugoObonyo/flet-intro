f2d40f81b1e2d6e3130967b24e1005d45d31114f
OyugoObonyo <nyikal1999@gmail.com>
2022-09-03 12:25:33 +0300

    Simple TODO

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+from hashlib import new
 import flet
-from flet import Page, Text, TextField, Row, ElevatedButton
+from flet import Page, Text, TextField, Row, ElevatedButton, Checkbox
 
 def main(page: Page):
     # page.add(x) can be used in place of page.controls.append(x)
-    def button_clicked(event):
-        page.add(Text("CLICKED"))
-        print("CLICKED and e is: ", event)
-
-    page.add(
-        Row(controls = [
-            TextField(label="What is your name?"),
-            ElevatedButton(text="Say my name!", on_click=button_clicked)
-        ])
-    )
+    def add_clicked(event):
+        page.add(Checkbox(label=new_task.value))
+    
+    new_task = TextField(hint_text="Add a task", width = 300)
+    page.add(Row([
+        new_task,
+        ElevatedButton("Add", on_click=add_clicked)
+        ]))
     page.update()
 
 
-flet.app(target=main, view=flet.WEB_BROWSER)
+flet.app(target=main)
