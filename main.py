8266053df57ad097588385ea0ca9d88e0375747a
OyugoObonyo <nyikal1999@gmail.com>
2022-11-11 11:31:06 +0300

    implement listview

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
 import flet
-from flet import(
-    Page, TextField,
-    Row, ElevatedButton,
-    Checkbox, Text, Column,
-    IconButton, icons
-)
+from flet import ListView, Page, Text
 
-# To run your flet app in hot reload mode,
-# run flet name_of_entry_point.py -d 
 def main(page: Page):
+    lv = ListView(height=250, spacing=15)
+    for i in range(5000):
+        lv.controls.append(Text(f"Line {i}"))
+    page.add(lv)
 
-    page.title = "TODO App"
-    page.horizontal_alignment = "center"
-    heading = Text(
-        value="Provide your full name",
-        size=30,
-        color="blue",
-        weight="bold")
-    first_name = TextField(label="First name", text_align="right", autofocus=True, width=300)
-    last_name = TextField(label="Last name", width=300)
-
-    def button_clicked(e):
-        if not first_name.value:
-            first_name.error_text = "First name cannot be blank"
-            page.update()
-        if not last_name.value:
-            last_name.error_text = "Last name cannot be blank"
-            page.update()
-        else:
-            page.add(Text(f"Hello {first_name.value} {last_name.value}!"))
-    
-    accept_button = IconButton(icons.LOGOUT_SHARP , icon_color="blue", on_click=button_clicked)
-
-    page.add(
-        heading,
-        first_name,
-        last_name,
-        accept_button
-    )
-    page.update()
-
-if __name__ == "__main__":
-    flet.app(target=main, port=5500, view=flet.WEB_BROWSER)
+flet.app(target=main, view=flet.WEB_BROWSER)
